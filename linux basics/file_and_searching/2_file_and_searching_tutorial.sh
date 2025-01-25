72076350fe24005df882a27373309d575eb0e90e
cy-sec-chef <115705829+ROT101@users.noreply.github.com>
2025-01-25 12:34:25 +0200

    Update and rename 2_file_and_searching_tutorial.md to 2_file_and_searching_tutorial.sh

--- linux basics/file_and_searching/2_file_and_searching_tutorial.sh
+++ linux basics/file_and_searching/2_file_and_searching_tutorial.sh
@@ -1,4 +1,4 @@
-```bash
+#!/bin/env bash   
 # Create a new file called "example.txt" with some content
 echo "Hello World!" > example.txt
 
@@ -13,4 +13,4 @@ sed -i 's/World/Universe/g' example.txt
 
 # Open the file in the nano editor
 nano example.txt
-```
+
